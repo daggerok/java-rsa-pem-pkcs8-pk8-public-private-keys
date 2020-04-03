36757f119ea6241ef704b70445fcb869a19d0ff4
Maksim Kostromin <daggerok@gmail.com>
2020-04-03 22:17:01 +0300

    yolo push (C) whatthecommit.com

--- consume-keys-files/src/main/java/consumekeysfiles/Main.java
+++ consume-keys-files/src/main/java/consumekeysfiles/Main.java
@@ -3,7 +3,6 @@ package consumekeysfiles;
 import lombok.SneakyThrows;
 
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.KeyFactory;
@@ -39,51 +38,77 @@ public class Main {
    */
   @SneakyThrows
   public static void main(String[] args) {
-
-    var in = Arrays.stream(args)
-                   .map(s -> s.split("="))
-                   .filter(strings -> strings.length == 2)
-                   .collect(Collectors.toMap(strings -> strings[0],
-                                             strings -> strings[1]));
-
-    var publicKeyParam = in.getOrDefault("--public", "public_key.pem");
-    var privateKeyParam = in.getOrDefault("--private", "private_key.pem");
-
-    var pathToPublic = Paths.get(publicKeyParam);
-    var pathToPrivate = Paths.get(privateKeyParam);
-
-    // var systemCharset = StandardCharsets.UTF_8;
-    var systemCharset = Charset.defaultCharset();
-    log.info("charset: '" + systemCharset.toString() + "'");
-    var publicKeyRawContent = Files.readString(pathToPublic, systemCharset);
-    var privateKeyRawContent = Files.readString(pathToPrivate, systemCharset);
-
+    // environment
+    var systemCharset = Charset.defaultCharset(); // StandardCharsets.UTF_8
     var lineSeparator = System.lineSeparator();
     log.info("line separator: '" + lineSeparator + "'");
+    log.info("charset: '" + systemCharset.toString() + "'");
     // var lineSeparator = System.getProperty("os.name").toLowerCase().contains("win") ? "\\r\\n" : "\\n";
-    var publicKey = publicKeyRawContent // fix Illegal base64 characters...
-                                       .replaceAll("-----BEGIN PUBLIC KEY-----", "")
-                                       .replaceAll("-----END PUBLIC KEY-----", "")
-                                        // .replaceAll("$|\n|\r\n|\n |\r\n | \n| \r\n", "")
-                                        .replaceAll(lineSeparator, "");
-    log.info(publicKey);
-    var privateKey = privateKeyRawContent.replaceAll("-----BEGIN PRIVATE KEY-----", "")
-                                         .replaceAll("-----END PRIVATE KEY-----", "")
-                                         // .replaceAll("$|\n|\r\n|\n |\r\n | \n| \r\n", "")
-                                         .replaceAll(lineSeparator, "");
-    log.info(privateKey);
 
+    // infrastructure
     var decoder = Base64.getDecoder();
-    var x509EncodedPublicKeySpec = new X509EncodedKeySpec(decoder.decode(publicKey));
-    var pkcs8EncodedPrivateKeySpec = new PKCS8EncodedKeySpec(decoder.decode(privateKey));
-
     var keyFactory = KeyFactory.getInstance("RSA");
-    var rsaPublicKey = (RSAPublicKey) keyFactory.generatePublic(x509EncodedPublicKeySpec);
-    var rsaPrivateKey = (RSAPrivateKey) keyFactory.generatePrivate(pkcs8EncodedPrivateKeySpec);
 
-    log.info("RSA public key:");
-    log.info(rsaPublicKey.toString());
-    log.info("RSA private key:");
-    log.info(rsaPrivateKey.toString());
+    // passed program arguments
+    var arguments = Arrays.stream(args)
+                          .map(s -> s.split("="))
+                          .filter(strings -> strings.length == 2)
+                          .collect(Collectors.toMap(strings -> strings[0],
+                                                    strings -> strings[1]));
+
+    // public key
+    var publicKeyPath = arguments.getOrDefault("--public", "public_key.pem");
+    var publicKey = getPublicKey(publicKeyPath, systemCharset, lineSeparator);
+    var rsaPublicKey = getRsaPublicKey(publicKey, decoder, keyFactory);
+
+    log.info("Public key algorithm: " + rsaPublicKey.getAlgorithm());
+    log.info("Public key format: " + rsaPublicKey.getFormat());
+    log.info(publicKey);
+
+    // private key
+    var privateKeyPath = arguments.getOrDefault("--private", "private_key.pem");
+    var privateKey = getPrivateKey(privateKeyPath, systemCharset, lineSeparator);
+    var rsaPrivateKey = getRsaPrivateKey(privateKey, decoder, keyFactory);
+
+    // log.info(rsaPrivateKey.toString());
+    log.info("Private key algorithm: " + rsaPrivateKey.getAlgorithm());
+    log.info("Private key format: " + rsaPrivateKey.getFormat());
+    // log.info("  => modulus: " + rsaPrivateKey.getModulus());
+    // log.info("  => exponent: " + rsaPrivateKey.getPrivateExponent());
+    // log.info("  => params: " + rsaPrivateKey.getParams());
+    log.info(privateKey);
+  }
+
+  @SneakyThrows
+  static String getPublicKey(String publicKeyPath, Charset systemCharset, String lineSeparator) {
+    var pathToPublic = Paths.get(publicKeyPath);
+    var publicKeyRawContent = Files.readString(pathToPublic, systemCharset); // SneakyThrows
+    return /* var publicKey = */ publicKeyRawContent // fix Illegal base64 characters...
+                                                    .replaceAll("-----BEGIN PUBLIC KEY-----", "")
+                                                    .replaceAll("-----END PUBLIC KEY-----", "")
+                                                    // .replaceAll("$|\n|\r\n|\n |\r\n | \n| \r\n", "")
+                                                    .replaceAll(lineSeparator, "");
+  }
+
+  @SneakyThrows
+  static RSAPublicKey getRsaPublicKey(String publicKey, Base64.Decoder decoder, KeyFactory keyFactory) {
+    var x509EncodedPublicKeySpec = new X509EncodedKeySpec(decoder.decode(publicKey));
+    return /* var rsaPublicKey = */ (RSAPublicKey) keyFactory.generatePublic(x509EncodedPublicKeySpec); // SneakyThrows
+  }
+
+  @SneakyThrows
+  static String getPrivateKey(String privateKeyPath, Charset systemCharset, String lineSeparator) {
+    var pathToPrivate = Paths.get(privateKeyPath);
+    var privateKeyRawContent = Files.readString(pathToPrivate, systemCharset); // SneakyThrows
+    return /* var privateKey = */ privateKeyRawContent.replaceAll("-----BEGIN PRIVATE KEY-----", "")
+                                                      .replaceAll("-----END PRIVATE KEY-----", "")
+                                                      // .replaceAll("$|\n|\r\n|\n |\r\n | \n| \r\n", "")
+                                                      .replaceAll(lineSeparator, "");
+  }
+
+  @SneakyThrows
+  static RSAPrivateKey getRsaPrivateKey(String privateKey, Base64.Decoder decoder, KeyFactory keyFactory) {
+    var pkcs8EncodedPrivateKeySpec = new PKCS8EncodedKeySpec(decoder.decode(privateKey));
+    return /* var rsaPrivateKey = */ (RSAPrivateKey) keyFactory.generatePrivate(pkcs8EncodedPrivateKeySpec); // SneakyThrows
   }
 }
