373bf68969a00e50ad87b51b6076bf147c0cb55a
Maksim Kostromin <daggerok@gmail.com>
2020-04-29 23:46:02 +0300

    needs more cow bell (C) whatthecommit.com

--- rsa-asymmetric-cryptography/src/main/java/daggerok/Args.java
+++ rsa-asymmetric-cryptography/src/main/java/daggerok/Args.java
@@ -5,9 +5,7 @@ import lombok.Value;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 /**
  * Parsing program arguments.
@@ -17,28 +15,26 @@ public class Args {
 
   String[] args;
 
-  public boolean is(String key) {
+  public boolean hasCommand(String key) {
     Optional.ofNullable(key)
             .filter(Predicate.not(String::isBlank))
-            .orElseThrow(() -> new IllegalArgumentException("key may not be null or empty."));
-    return Arrays.asList(Objects.requireNonNull(args, "args may not be null."))
+            .orElseThrow(Err.reThrow.apply("key may not be null or empty"));
+    return Arrays.asList(Objects.requireNonNull(args, "args may not be null"))
                  .contains(key);
   }
 
-  public Optional<String> find(String flag) {
-    return Optional.ofNullable(get(flag, null));
+  public Optional<String> find(String query) {
+    return Optional.ofNullable(find(query, null));
   }
 
-  public String get(String flag, String defaultValue) {
-    final Function<String, Supplier<RuntimeException>> err = name -> () ->
-        new RuntimeException(String.format("%s may not be null or empty.", name));
-    var aFlag = Optional.ofNullable(flag)
-                        .filter(Predicate.not(String::isBlank))
-                        .orElseThrow(err.apply("key"));
+  public String find(String query, String defaultValue) {
+    Optional.ofNullable(query)
+            .filter(Predicate.not(String::isBlank))
+            .orElseThrow(Err.reThrow.apply("query may not be null or empty"));
     return Arrays.stream(args)
                  .filter(s -> s.startsWith("--") && s.contains("="))
                  .map(s -> s.split("="))
-                 .filter(pair -> pair[0].endsWith(aFlag))
+                 .filter(pair -> pair[0].endsWith(query))
                  .findFirst()
                  .map(pair -> pair[1])
                  .orElse(defaultValue);
--- rsa-asymmetric-cryptography/src/main/java/daggerok/Main.java
+++ rsa-asymmetric-cryptography/src/main/java/daggerok/Main.java
@@ -8,32 +8,32 @@ public class Main {
     var publicKeyPath = arguments.find("publicKey").orElse("target/my/public.key");
     var privateKeyPath = arguments.find("privateKey").orElse("target/my/private.key");
 
-    if (arguments.is("generate")) {
+    if (arguments.hasCommand("generate")) {
 
       var keyPair = KeyService.generateKeyPair(4096);
       var publicKey = KeyService.generatePublicKey(keyPair);
       var privateKey = KeyService.generatePrivateKey(keyPair);
 
-      if (!arguments.is("and")) return;
+      if (!arguments.hasCommand("and")) return;
 
-      if (arguments.is("write")) {
+      if (arguments.hasCommand("write")) {
         KeyService.write(publicKey, publicKeyPath);
         KeyService.write(privateKey, privateKeyPath);
       }
     }
 
-    if (arguments.is("read")) {
+    if (arguments.hasCommand("read")) {
       System.out.println(KeyService.readPublicKey(publicKeyPath));
       System.out.println(KeyService.readPrivateKey(privateKeyPath));
     }
 
-    var payload = arguments.find("payload");
+    var query = arguments.find("payload");
 
-    if (arguments.is("encrypt")) payload
+    if (arguments.hasCommand("encrypt")) query
         .map(value -> KeyService.encrypt(value, privateKeyPath))
         .ifPresent(System.out::println);
 
-    if (arguments.is("decrypt")) payload
+    if (arguments.hasCommand("decrypt")) query
         .map(value -> KeyService.decrypt(value, publicKeyPath))
         .ifPresent(System.out::println);
   }
